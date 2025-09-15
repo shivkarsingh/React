cfb36e0985c23f43c6b1ba68b703b2efd0f23660
shivkarsingh <shivkarcse@gmail.com>
2025-09-15 23:35:14 +0530

    bundlers

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const struct = React.createElement( "div", { id : "parent" },
-    [ React.createElement( "h1", { id : "child1" }, "heading1" ), React.createElement( "h1", { id : "child2" }, "heading2" ) ]
+    React.createElement( "h1", { id : "child2" }, "heading2" )
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(struct);
-
 
 

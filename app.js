6d2951af8cb4392021bed8cefe3413156d263c2b
shivkarsingh <shivkarcse@gmail.com>
2025-09-24 23:33:38 +0530

    working

--- app.js
+++ app.js
@@ -1,12 +1,65 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom/client"
 
-const struct = React.createElement( "div", { id : "parent" },
-    React.createElement( "h1", { id : "child2" }, "heading2" )
-);
+const Header = () => {
+    return(
+
+        <div className="header">
+
+            <div className="logo-container">
+                <img className="logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUCIXB7HgsGHPLbgLzCEIOLCH-c9F78GIETg&s" alt="logo"/>
+            </div>
+
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+
+    );
+}
+
+const RestaurantCard = () => {
+    return(
+
+        <div className="res-card">
+            <h3> Megahna Foods </h3>
+        </div>
+    );
+}
+
+const Body = () => {
+    return (
+        <div className="body">
+
+            <div className="search">
+                Search
+            </div>
+
+            <div className="res-container">
+                <RestaurantCard/>
+            </div>
+        </div>
+    );
+}
+
+const AppLayout = () => {
+    return(
+        
+        <div className="app">
+
+            <Header/>
+            <Body/>
+
+
+
+        </div>
+    );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(struct);
-
-
+root.render(<AppLayout/>);
